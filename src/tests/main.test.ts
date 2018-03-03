1ec8db770c392b121950659440ccd6b42ee2dcfb
niuba <niuba.zbz@gmail.com>
2018-03-03 13:10:07 +0800

    update test
    Signed-off-by: niuba <niuba.zbz@gmail.com>

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -44,31 +44,30 @@ IPostUpdate
 
 }
 
-const testRes = new TestRes();
+describe("test MockAdapter", () => {
+  const testRes = new TestRes();
 
-
-test("create", () => {
-  return testRes.create({
-    title: "new title",
-  }).toPromise().then((data) => {
-    expect(data.id).toBe(2);
-    expect(data.title).toBe("new title");
+  test("create", () => {
+    return testRes.create({
+      title: "new title",
+    }).toPromise().then((data) => {
+      expect(data.id).toBe(2);
+      expect(data.title).toBe("new title");
+    });
+  });
+  test("get", () => {
+    return testRes.get(1).toPromise().then((data) => {
+      expect(data.id).toBe(1);
+      expect(data.title).toBe("title");
+    });
+  });
+  test("update", () => {
+    return testRes.update({
+      id: 1,
+      title: "change title",
+    }).toPromise().then((data) => {
+      expect(data.id).toBe(1);
+      expect(data.title).toBe("change title");
+    });
   });
 });
-test("get", () => {
-  return testRes.get(1).toPromise().then((data) => {
-    expect(data.id).toBe(1);
-    expect(data.title).toBe("title");
-  });
-});
-test("update", () => {
-  return testRes.update({
-    id: 1,
-    title: "change title",
-  }).toPromise().then((data) => {
-    expect(data.id).toBe(1);
-    expect(data.title).toBe("change title");
-  });
-});
-
-
